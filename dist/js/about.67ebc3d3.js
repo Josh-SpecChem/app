a1762a8c6e9e9bff39a60df2c5d32c856decf1c0
Josh-SpecChem <106373289+Josh-SpecChem@users.noreply.github.com>
2022-08-17 15:47:23 -0500

    Add files via upload

---
+++ dist/js/about.67ebc3d3.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["about"],{f820:function(t,e,a){"use strict";a.r(e);var n=function(){var t=this,e=t.$createElement,a=t._self._c||e;return a("div",{staticClass:"about"},[a("p",[a("router-link",{attrs:{to:"/"}},[t._v("<<")])],1),a("h1",[t._v("This is an about page")]),a("p",[t._v("We have some pretty cool Stuff")])])},o=[],s=a("2877"),u={},l=Object(s["a"])(u,n,o,!1,null,null,null);e["default"]=l.exports}}]);
+//# sourceMappingURL=about.67ebc3d3.js.map
---
+++ dist/precache-manifest.4e2d511bd5962c7ad5df9e7a965cfe51.js
@@ -0,0 +1,222 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a27a4163254fc8fce870c8cc3a3f94f",
+    "url": "/404.html"
+  },
+  {
+    "revision": "71d1a3d07cf377d84b6dbfba681b52ec",
+    "url": "/docs/crossover-chart.csv"
+  },
+  {
+    "revision": "326ac5318174497d78e52863ad99b4a2",
+    "url": "/fonts/Butler/Butler_Black.otf"
+  },
+  {
+    "revision": "c256137d05a1f0c5faa6d804c68b8c33",
+    "url": "/fonts/Butler/Butler_Black_Stencil.otf"
+  },
+  {
+    "revision": "c7e8afa352a74142f3e99a085d28e57b",
+    "url": "/fonts/Butler/Butler_Bold.otf"
+  },
+  {
+    "revision": "b2d5c68a3401c3c72644c3a229cd1b9f",
+    "url": "/fonts/Butler/Butler_Bold_Stencil.otf"
+  },
+  {
+    "revision": "bbd0c656db3e9d6b15c03d013aa64a54",
+    "url": "/fonts/Butler/Butler_ExtraBold.otf"
+  },
+  {
+    "revision": "c6815e57c45ca335854a77a3804751fa",
+    "url": "/fonts/Butler/Butler_ExtraBold_Stencil.otf"
+  },
+  {
+    "revision": "69701596bbadfac6a07f3c5eb8679f8a",
+    "url": "/fonts/Butler/Butler_Light.otf"
+  },
+  {
+    "revision": "1d115cd1a8d3cd8996e2408434ea2715",
+    "url": "/fonts/Butler/Butler_Light_Stencil.otf"
+  },
+  {
+    "revision": "cb24819ed2e3c58727933dd9c9f205e8",
+    "url": "/fonts/Butler/Butler_Medium.otf"
+  },
+  {
+    "revision": "2903b51aa318bdec3edd32544a8a921d",
+    "url": "/fonts/Butler/Butler_Medium_Stencil.otf"
+  },
+  {
+    "revision": "1f0cec9920b2a8aacef20eb306f92f7c",
+    "url": "/fonts/Butler/Butler_Regular.otf"
+  },
+  {
+    "revision": "824f8011f1f2d34aae9e68020459f884",
+    "url": "/fonts/Butler/Butler_Regular_Stencil.otf"
+  },
+  {
+    "revision": "9d292bf3bed6f18de36d9e7ce5b4cc5d",
+    "url": "/fonts/Butler/Butler_Ultra_Light.otf"
+  },
+  {
+    "revision": "263d3335aef9436fdcbc7b433c4cf092",
+    "url": "/fonts/Butler/Butler_Ultra_Light_Stencil.otf"
+  },
+  {
+    "revision": "2312dfe04a98e87ddb08725a6d6d565a",
+    "url": "/fonts/futura_heavy/futura_heavy.ttf"
+  },
+  {
+    "revision": "98b79d457131d3150e5ef025a75d1637",
+    "url": "/fonts/thinking_of_betty/Read Me.rtf"
+  },
+  {
+    "revision": "f81704760ce44db49b3e29ba1e38b1ab",
+    "url": "/fonts/thinking_of_betty/Thinking_of_Betty.ttf"
+  },
+  {
+    "revision": "e8cc196c821566ab234f38420138dc93",
+    "url": "/fonts/thinking_of_betty/thinking_of_betty.png"
+  },
+  {
+    "revision": "00a391753fb9d3c0b8670d260fc66dff",
+    "url": "/img/SpecChem-logo.png"
+  },
+  {
+    "revision": "5a23c534b66ac56688aa705a1020c0c4",
+    "url": "/img/back.jpg"
+  },
+  {
+    "revision": "c8187fba9a01c55d0843a99973df2ce5",
+    "url": "/img/background.png"
+  },
+  {
+    "revision": "e7196f3667c05ea92363eac3b1ea0cc7",
+    "url": "/img/background2.png"
+  },
+  {
+    "revision": "e7196f3667c05ea92363eac3b1ea0cc7",
+    "url": "/img/bluetexture.png"
+  },
+  {
+    "revision": "90a504cec54ce340053d93b390388dd8",
+    "url": "/img/bottom-border-fade.png"
+  },
+  {
+    "revision": "bb2bef3cf233dd6264e4d2fad3c34060",
+    "url": "/img/button-white.png"
+  },
+  {
+    "revision": "a98aa316a9ace18846244f7fd50bfd21",
+    "url": "/img/calculate.png"
+  },
+  {
+    "revision": "475429d48f89b6cccafde3aeb8e24317",
+    "url": "/img/calculator.png"
+  },
+  {
+    "revision": "18fc71a7254f9529e2dbd11ee3221a64",
+    "url": "/img/circle.png"
+  },
+  {
+    "revision": "22fcdf18f3efb0c510e21ba73d2d3f10",
+    "url": "/img/crossover-chart.png"
+  },
+  {
+    "revision": "789249a094ed94a022f92499fc75f6b9",
+    "url": "/img/dot-approvals.png"
+  },
+  {
+    "revision": "f229da62288521ed0298f155ef0f4b6c",
+    "url": "/img/dropdown-lg.png"
+  },
+  {
+    "revision": "250c63b9c8959921f8689ff0f4836cef",
+    "url": "/img/dropdown-md-2.png"
+  },
+  {
+    "revision": "76648923fbc142a4c9d2335de9bc19e4",
+    "url": "/img/dropdown-md.png"
+  },
+  {
+    "revision": "44c8879bb5ac221acf5bfbc706d053db",
+    "url": "/img/dropdown-sm.png"
+  },
+  {
+    "revision": "da7482baaf478a68c7c6295890770f1c",
+    "url": "/img/giphy.webp"
+  },
+  {
+    "revision": "f79b4811f8f435f6072a4e7392b3947e",
+    "url": "/img/home.jpg"
+  },
+  {
+    "revision": "49ebe3ec740ef29f001b3ba639352a74",
+    "url": "/img/loading.gif"
+  },
+  {
+    "revision": "d22c1e8f75779f8941dad08292a25644",
+    "url": "/img/menu-close.jpg"
+  },
+  {
+    "revision": "d2c92562b5bdc0bd5da2ccb45f20a6ed",
+    "url": "/img/menu.jpg"
+  },
+  {
+    "revision": "ab9e3e7a0083363f667284a6c8d5ded6",
+    "url": "/img/product-library.png"
+  },
+  {
+    "revision": "af360afb7cbefaa6500e51340f03aabd",
+    "url": "/img/specchem-product-circle.png"
+  },
+  {
+    "revision": "4f9736fdaee1d452c1d759e0f5ec707b",
+    "url": "/img/textbox-md.png"
+  },
+  {
+    "revision": "a9b9542281319bb552a44baa53fd5ec0",
+    "url": "/img/textbox-sm.png"
+  },
+  {
+    "revision": "1b2c2ef5cc6e950e0131d51ad4eb73c1",
+    "url": "/img/textbox-ssm.png"
+  },
+  {
+    "revision": "b71e8470531d27efa30a7becf13fb12a",
+    "url": "/img/video-gallery.png"
+  },
+  {
+    "revision": "2d6f91a3fb07415afae79ec46a31d719",
+    "url": "/img/white-background.png"
+  },
+  {
+    "revision": "4ed39189f3ee9212feef3be61b48a8a9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "aac42840a5155deb38e7",
+    "url": "/js/about.67ebc3d3.js"
+  },
+  {
+    "revision": "96d449802b00e2eb9eae",
+    "url": "/js/app.b591fd93.js"
+  },
+  {
+    "revision": "4ef8b63a7f478817a3b2",
+    "url": "/js/chunk-vendors.bf8b63ac.js"
+  },
+  {
+    "revision": "9727f60113e2192fda32df13900040d2",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "35607e8cf5dcbcbb35a5e64015d8a786",
+    "url": "/styles.css"
+  }
+]);
